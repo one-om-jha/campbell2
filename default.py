b0071c0f754494a388c009c3b509f7ffce1cab42
terminalScript <omjha23@gmail.com>
2021-03-01 19:45:23 -0500

    Actually init

---
+++ default.py
@@ -0,0 +1,48 @@
+# Import discord.py library
+import discord
+
+# gets client object from discord.py
+bot = discord.Client()
+
+# How this works! (for future me)
+# utilizes discord.py
+# https://discordpy.readthedocs.io/en/latest/
+
+# Event Listeners - indicated by @bot.event
+# on_ready()    - bot online
+# on_message()  - detects message sent in channel
+
+# on_message()
+# returns Message object when message is detected
+# https://discordpy.readthedocs.io/en/latest/api.html#message
+
+# Event Listener - Bot Online
+@bot.event
+async def on_ready():
+    print("Campbell 2 is online.")
+
+# Event Listener - New Message Detected
+@bot.event
+async def on_message(message):
+    if message.content == "hi campbell":
+        await message.channel.send("hello")
+
+    # Check for Prefix >
+    if message.content[0:1] == ">":
+        # Assign content after prefix to command string
+        command = message.content[1:]
+
+        if command[:3] == "gif":
+            await message.channel.send(command_gif)
+
+
+def command_gif(tag):
+    gifList = {
+        "general": "img/general.gif"
+    }
+    error_message = "There is no gif with that name."
+    return gifList.get(tag, error_message)
+    
+
+# execute bot with token
+bot.run("MzIwNzI4NTQ5OTY1Mjk5NzEz.WTNbvQ.WLuFJN2dQlp760eSAcxie04xPsg")
