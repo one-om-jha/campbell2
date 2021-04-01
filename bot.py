297f1b55b23d46c7353e3846a260421f62a8beef
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 15:37:38 -0400

    bot

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # Commands
 
 @bot.command()
-async def gif(ctx, arg):
+async def gif(ctx, gif):
     gif_list = {
             "general":      "img/general.gif",
             "gifs":             "img/gifs.gif",
@@ -28,7 +28,17 @@ async def gif(ctx, arg):
             "ballsitch":        "img/ballsitch.gif",
             "this":             "img/this.gif"
             }
-    await ctx.send(gif_list.get(arg);
+    await ctx.send(gif_list.get(gif);
+
+@bot.command()
+async def flex(ctx, mention: Member):
+    reply = "{0.mention} got caught flexing in {1.name} by {0.mention}. Bitch.".format(mention, ctx.guild, ctx.author)
+    await ctx.send(reply)
+
+@bot.command()
+async def sus(ctx, mention: Member):
+    reply = "{0.mention} ayo son? wtf.".format(mention)
+    await ctx.send(reply)
 
 # execute bot with token
 bot.run(DISCORD_TOKEN)
