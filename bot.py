b46336964d49c75547204528643249aa63f84362
BuildTools <unconfigured@null.spigotmc.org>
2021-04-01 15:49:16 -0400

    bot replies

--- bot.py
+++ bot.py
@@ -14,6 +14,33 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 # Commands
 
+@bot.event()
+async def on_message():
+    # Run abstract reactions
+    reactions(message)
+
+    # Run abstract replies
+    replies(message)
+
+async def reactions(message: Message):
+    react_list = {
+            "fax":      "<:Fax:816331584999391262>",
+            "this!":    "<:this:823714994424250368>",
+            "dababy":   "<:dababy:827036898504736788>"
+            }
+    content = message.content.lower()
+    for key in (key in react_list if key in content):
+        await message.add_reaction(react_list.get(key))
+
+async def replies(message: Message):
+    reply_list = {
+            "thisdge":      "https://cdn.discordapp.com/attachments/189827180610453505/822571054773174282/unknown.png"
+            }
+    content = message.content.lower()
+    for key in (key in reply_list if key in content):
+        await message.reply(reply_list.get(key))
+
+
 @bot.command()
 async def gif(ctx, gif):
     gif_list = {
