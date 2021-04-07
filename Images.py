386235bfe1356cb13b2602127f121faf3dce82dc
BuildTools <unconfigured@null.spigotmc.org>
2021-04-07 16:40:42 -0400

    does not exist stuff

--- Images.py
+++ Images.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import tasks, commands
+import io
+import aiohttp
+import random
 
 image_list = {
         "timezone": "https://cdn.discordapp.com/attachments/189827180610453505/828661635504799790/Time_Zones.jpg",
@@ -14,3 +17,32 @@ class Images(commands.Cog):
     @commands.command()
     async def img(self, ctx, img):
         await ctx.send(image_list.get(img))
+
+    @commands.command()
+    async def horse(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://thishorsedoesnotexist.com') as resp:
+                if resp.status != 200:
+                    return await ctx.send("Could not get file...")
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'horse.jpg'))
+
+    @commands.command()
+    async def waifu(self, ctx):
+        num = random.randint(1,99999)
+        url = "https://www.thiswaifudoesnotexist.net/example-{0}.jpg".format(num)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    return await ctx.send("Could not get file...")
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'waifu.jpg'))
+
+    @commands.command()
+    async def art(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://thisartworkdoesnotexist.com') as resp:
+                if resp.status != 200:
+                    return await ctx.send("Could not get file...")
+                data = io.BytesIO(await resp.read())
+                await ctx.send(file=discord.File(data, 'art.jpg'))
