92f6c01efd3c3ab499554bbe4118bc072cb40ef2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 17:04:42 -0400

    hmm

--- AbstractResponses.py
+++ AbstractResponses.py
@@ -34,5 +34,6 @@ react_list = {
 
 reply_list = {
         "thisdge":      "https://cdn.discordapp.com/attachments/189827180610453505/822571054773174282/unknown.png",
-        "salaander":    "salaander"
+        "salaander":    "salaander",
+        "i cant quite find the words jaden, and i consider myself an erudite man, to describe the heinous acts of human cruelty that i would commit to fuck your sister. The precise and grueling words that my brain so desperately tongues around in my memory to find that would accurately describe, in detail, exactly what i would do to your sister's guts, evade me. And yet, I do not hesitate to emphasize my point enough. Given the opportunity, I would not relent. The sky would blacken and hell would freeze over before the concept of stopping would even begin to creep into the annals of my mind. \"Stop what?\" you might ask, and to that I answer that I am not exactly sure what. My brain fails to comprehend the true nature of this action, or what it would look like, all I know is that no amount of it could ever satisfy me.":   "what the actual fuck my son"
         }
